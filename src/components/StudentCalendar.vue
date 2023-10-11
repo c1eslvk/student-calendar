de51e70f45f07f60b117ccca4afe79b731143eab
Maksymilian Cieślak <maks.cieslak13@gmail.com>
2023-10-11 15:14:36 +0200

    small changes

--- src/components/StudentCalendar.vue
+++ src/components/StudentCalendar.vue
@@ -5,32 +5,18 @@
           <v-toolbar
             flat
           >
-            <v-btn
-              outlined
-              class="mr-4"
-              color="grey darken-2"
-              @click="setToday"
-            >
+            <v-btn color="primary" class="mr-4" @click="dialog = true" dark>
+              New Event
+            </v-btn>
+            <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
               Today
             </v-btn>
-            <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="prev"
-            >
+            <v-btn fab text small color="grey darken-2" @click="prev">
               <v-icon small>
                 mdi-chevron-left
               </v-icon>
             </v-btn>
-            <v-btn
-              fab
-              text
-              small
-              color="grey darken-2"
-              @click="next"
-            >
+            <v-btn fab text small color="grey darken-2" @click="next">
               <v-icon small>
                 mdi-chevron-right
               </v-icon>
@@ -39,17 +25,9 @@
               {{ $refs.calendar.title }}
             </v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-menu
-              bottom
-              right
-            >
+            <v-menu bottom right>
               <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  outlined
-                  color="grey darken-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
+                <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
                   <span>{{ typeToLabel[type] }}</span>
                   <v-icon right>
                     mdi-menu-down
@@ -65,9 +43,6 @@
                 </v-list-item>
                 <v-list-item @click="type = 'month'">
                   <v-list-item-title>Month</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = '4day'">
-                  <v-list-item-title>4 days</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
@@ -86,25 +61,13 @@
             @click:date="viewDay"
             @change="updateRange"
           ></v-calendar>
-          <v-menu
-            v-model="selectedOpen"
-            :close-on-content-click="false"
-            :activator="selectedElement"
-            offset-x
-          >
-            <v-card
-              color="grey lighten-4"
-              min-width="350px"
-              flat
-            >
-              <v-toolbar
-                :color="selectedEvent.color"
-                dark
-              >
+          <v-menu v-model="selectedOpen" :close-on-content-click="false" :activator="selectedElement" offset-x>
+            <v-card color="grey lighten-4" min-width="350px" flat>
+              <v-toolbar :color="selectedEvent.color" dark>
                 <v-btn icon>
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
-                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <v-toolbar-title>{{ selectedEvent.name }}</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-btn icon>
                   <v-icon>mdi-heart</v-icon>
@@ -117,11 +80,7 @@
                 <span v-html="selectedEvent.details"></span>
               </v-card-text>
               <v-card-actions>
-                <v-btn
-                  text
-                  color="secondary"
-                  @click="selectedOpen = false"
-                >
+                <v-btn text color="secondary" @click="selectedOpen = false">
                   Cancel
                 </v-btn>
               </v-card-actions>
@@ -163,15 +122,114 @@ import { db } from '@/main.js'
     },
     methods: {
         async getEvents() {
-            let snapshot = await db.collection('calEvent').get();
-            let events = [];
-            snapshot.forEach(doc => {
-                let appData = doc.data();
-                appData.id = doc.id;
-                events.push(appData);
+              let snapshot = await db.collection('calEvent').get();
+              let events = [];
+              snapshot.forEach(doc => {
+                  let appData = doc.data();
+                  appData.id = doc.id;
+                  events.push(appData);
+              });
+              this.events = events;
+        },
+        async updateEvent(event) {
+          await db.collection('calEvent').doc(this.currentlyEditing).update({
+            details: event.details
+          });
+          this.selectedOpen = false;
+          this.currentlyEditing = null;
+        },
+        async deleteEvent(event) {
+          await db.collection('calEvent').doc(event).delete();
+          this.selectedOpen = false;
+          this.getEvents();
+        },
+        async addEvent() {
+          if (this.name && this.start && this.end) {
+            await db.collection('calEvent').add({
+              name: this.name,
+              details: this.details,
+              start: this.start,
+              end: this.end,
+              color: this.color
             });
-            this.events = events;
-        }
+            this.getEvents()
+            this.name = "";
+            this.details = "";
+            this.start = "";
+            this.end = "";
+            this.color = "#1976D2";
+
+          } else {
+            alert('Name, start and end date are required.');
+          }
+        },
+        viewDay ({ date }) {
+          this.focus = date;
+          this.type = 'day';
+        },
+        getEventColor (event) {
+          return event.color
+        },
+        setToday () {
+          this.focus = ''
+        },
+        prev () {
+          this.$refs.calendar.prev()
+        },
+        next () {
+          this.$refs.calendar.next()
+        },
+        showEvent ({ nativeEvent, event }) {
+          const open = () => {
+            this.selectedEvent = event
+            this.selectedElement = nativeEvent.target
+
+            this.focus = event.start.substr(0, 10);
+
+            requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+          }
+
+          if (this.selectedOpen) {
+            this.selectedOpen = false
+            requestAnimationFrame(() => requestAnimationFrame(() => open()))
+          } else {
+            open()
+          }
+
+          nativeEvent.stopPropagation()
+        },
+        editEvent(event) {
+          this.currentlyEditing = event.id;
+        },
+        updateRange ({ start, end }) {
+          const events = []
+
+          const min = new Date(`${start.date}T00:00:00`)
+          const max = new Date(`${end.date}T23:59:59`)
+          const days = (max.getTime() - min.getTime()) / 86400000
+          const eventCount = this.rnd(days, days + 20)
+
+          for (let i = 0; i < eventCount; i++) {
+            const allDay = this.rnd(0, 3) === 0
+            const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+            const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+            const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+            const second = new Date(first.getTime() + secondTimestamp)
+            
+            events.push({
+              name: this.names[this.rnd(0, this.names.length - 1)],
+              start: first,
+              end: second,
+              color: this.colors[this.rnd(0, this.colors.length - 1)],
+              timed: !allDay,
+            })
+          }
+
+          this.events = events
+        },
+        rnd (a, b) {
+          return Math.floor((b - a + 1) * Math.random()) + a
+        },
     }
 }
 </script>
